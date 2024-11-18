13a1793e5431e03f7237976e76f0129112716c05
Priscila Bueno de Souza <161883334+PriscilaB701@users.noreply.github.com>
2024-11-18 16:30:52 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,43 @@
 import socket
 import threading
 
-HOST = '127.0.0.1'
+HOST = '172.20.10.10'
 PORT = 1234
 LISTENER_LIMIT = 5
 active_clients = []
+
+def separaae(message, username):
+
+    if message.startswith('@'):
+        message = message[1:].strip()  #separa o @ do nome do destinatario
+
+    #divide a mensagem em duas partes: o nome do destinatário e o conteudo
+    parts = message.split(' ', 1)
+    if len(parts) < 2:
+        return None, None
+
+    target_user = parts[0].strip()  #destinatário
+    private_message = parts[1].strip()  #conteudo da mensagem privada
+
+
+    return target_user, private_message
+
+
+def mandanadm(target_user, private_message, username): #percorre todos os clientes para encontrar o destinatário
+    
+    for user, client in active_clients:
+        if user.lower() == target_user.lower():
+            msg_to_client(client, f"DM de {username}: {private_message}")
+    for sender_user, sender_client in active_clients:
+        if sender_user.lower() == username.lower():
+            msg_to_client(sender_client, f"Você enviou para {target_user}: {private_message}")
+            return
+
+    #se o usuário não for encontrado, envia uma mensagem de erro
+    for user, client in active_clients:
+        if user.lower() == username.lower():
+            msg_to_client(client, f"Usuário {target_user} não encontrado.")
+            return
 
 def listen_for_messages(client, username):
     while True:
@@ -18,7 +51,9 @@ def listen_for_messages(client, username):
                 if (username, client) in active_clients:
                     active_clients.remove((username, client))
                 break
-
+            elif message.startswith(('PRIVADO:','@')):
+                target_user, private_message = separaae(message, username)
+                mandanadm(target_user, private_message, username)
             elif message != '':
                 final_msg = f"{username}>{message}"
                 broadcast(final_msg)
@@ -33,6 +68,9 @@ def listen_for_messages(client, username):
             client.close()
             broadcast(f"{username} saiu do chat, erro de conexão")
             break
+
+
+
 
 
 
@@ -64,6 +102,10 @@ def client_handler(client):
             print('Username vazio')
             
     threading.Thread(target=listen_for_messages, args=(client, username)).start()
+
+
+
+
 
 
 def main():
